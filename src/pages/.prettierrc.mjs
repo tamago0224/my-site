ce772ec9b0e7076219cc3c700fb6dbe1d09ce516
tamago0224 <bigtreesasuke200733@gmail.com>
2023-11-15 08:36:13 +0900

    tutorial completed

---
+++ src/pages/.prettierrc.mjs
@@ -0,0 +1,14 @@
+// ref: https://github.com/withastro/prettier-plugin-astro
+// .prettierrc.mjs
+/** @type {import("prettier").Config} */
+export default {
+  plugins: ["prettier-plugin-astro"],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+};
